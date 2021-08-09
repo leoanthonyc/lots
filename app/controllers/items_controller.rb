3798982d6502c5621a2af51116599363dbc4c3c9
Leo Catane <leoanthony.catane@protonmail.com>
2021-08-09 20:38:18 +0800

    Add description rich_text

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,6 +45,6 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:name)
+    params.require(:item).permit(:name, :description)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
   validates :name,
     presence: true, 
     uniqueness: true
+
+  has_rich_text :description
 end
